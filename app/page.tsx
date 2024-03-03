5fc53b191367da3d573fe8b74fbb73c064641e52
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-03 17:10:50 +0800

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,18 @@
 "use client";
-import { Announcements, Navbar } from "@/components";
-import { useHomeStore, useItemStore } from "@/states";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useEffect, useMemo, useState } from "react";
+import { Navbar } from "@/components";
+import { useHomeStore } from "@/states";
+import { useEffect } from "react";
 import { HeroSection, ItemsList, SponsorsList } from "./components";
 
-interface ItemCardProps {
-  item: {
-    _id: string;
-    image: string;
-    name: string;
-    customerPrice: number;
-    // Add any other properties as needed
-  };
-  onClick: () => void;
-}
-
-const ItemCard: React.FC<ItemCardProps> = ({ item, onClick }) => (
-  <div className="item-card" onClick={onClick}>
-    <div className="card">
-      <Image
-        src={item.image}
-        width={400}
-        height={400}
-        alt={item.image}
-        className="item-image"
-      ></Image>
-      <p className="text-3xl font-bold px-4 py-2">{item.name}</p>
-      <p className="text-2xl px-4 py-2">₱{item.customerPrice}</p>
-    </div>
-  </div>
-);
 export default function Home() {
   const {
     setNavbarVisible,
     navbarVisible,
-    items,
     getItems,
     brandNewTanks,
     refillTanks,
     accessories,
   } = useHomeStore();
-  const router = useRouter();
 
   useEffect(() => {
     getItems({ page: 1, limit: 100 });
