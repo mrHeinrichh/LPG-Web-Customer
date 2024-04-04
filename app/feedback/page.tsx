38302dddd1c90e0aa96da10d594d6877e859cf10
Ian <kyutomana25@gmail.com>
2024-04-05 04:19:41 +0800

    update feedback ui

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, InputField, Navbar, StarRating } from "@/components";
+import { Button, Navbar } from "@/components";
 import { get, patch } from "@/config";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -17,13 +17,7 @@ export default function Feedback() {
     q5: "",
   });
 
-  const [q1rating, setq1rating] = useState<number>(0);
-  const [q2rating, setq2rating] = useState<number>(0);
-  const [q3rating, setq3rating] = useState<number>(0);
-  const [q4rating, setq4rating] = useState<number>(0);
-  const [q5rating, setq5rating] = useState<number>(0);
-
-  const [transaction, settransaction] = useState<any>(null);
+  const [transaction, setTransaction] = useState<any>(null);
   const [validationErrors, setValidationErrors] = useState({
     q1: false,
     q2: false,
@@ -38,13 +32,12 @@ export default function Feedback() {
 
   const getTransaction = async (id: string) => {
     const { data } = await get(`transactions?filter={"_id": "${id}"}`);
-    if (data.status == "success") {
-      settransaction(data.data[0]);
+    if (data.status === "success") {
+      setTransaction(data.data[0]);
     }
   };
 
-  const handleChange = (event: any) => {
-    const { name, value } = event.target;
+  const handleChange = (name: string, value: string) => {
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
     setValidationErrors((prevErrors) => ({ ...prevErrors, [name]: false }));
   };
@@ -61,64 +54,20 @@ export default function Feedback() {
     });
 
     if (hasErrors) {
-      // Display an error message or take other actions
       return;
     }
 
     const { data } = await patch(`transactions/${id}/feedback`, {
       feedback: [
-        {
-          answer: formData.q1,
-          question:
-            "How satisfied are you with the speed and responsiveness of our mobile/web application when browsing and making purchases?",
-          rate: q1rating,
-        },
-        {
-          answer: formData.q2,
-          question:
-            "How satisfied are you with the approval and speed of your transaction in the system?",
-          rate: q2rating,
-        },
-        {
-          answer: formData.q3,
-          question:
-            "How satisfied are you with the communication skills and punctuality of the delivery rider in delivering your LPG order?",
-          rate: q3rating,
-        },
-        {
-          answer: formData.q4,
-          question:
-            "On a scale of 1 to 5, how would you describe your overall experience using our mobile/web application to purchase LPG products?",
-          rate: q4rating,
-        },
-        {
-          answer: formData.q5,
-          question:
-            "Overall, how likely are you to recommend our mobile/web application to others based on your experience using it for LPG purchases?",
-          rate: q5rating,
-        },
-        
+        { question: "Question 1", answer: formData.q1 },
+        { question: "Question 2", answer: formData.q2 },
+        { question: "Question 3", answer: formData.q3 },
+        { question: "Question 4", answer: formData.q4 },
+        { question: "Question 5", answer: formData.q5 },
       ],
-      
     });
 
-    if (data.status == "success") router.push("/");
-  };
-
-  const setq1Rate = (rate: number) => {
-    setq1rating(rate);
-  };
-  const setq2Rate = (rate: number) => {
-    setq2rating(rate);
-  };
-  const setq3Rate = (rate: number) => {
-    setq3rating(rate);
-  };
-  const setq4Rate = (rate: number) => {
-    setq4rating(rate);
-  };
-  const setq5Rate = (rate: number) => {
-    setq5rating(rate);
+    if (data.status === "success") router.push("/");
   };
 
   return (
@@ -127,46 +76,135 @@ export default function Feedback() {
       <div className="flex flex-col gap-3 bg-white rounded-lg shadow-xl pr-24 pl-24 pt-10">
         {transaction && transaction.status === "Completed" && (
           <>
-            <p className="text-3xl font-bold">Help us improve!</p>
-            <InputField
-              name="q1"
-              placeholder="How satisfied are you with the speed and responsiveness of our mobile/web application when browsing and making purchases?"
-              onChange={handleChange}
-              error={validationErrors.q1 && "Field must be filled"}
-            />
-            <StarRating onClick={setq1Rate} rating={q1rating} />
+            <p className="text-3xl font-bold text-center">Help us improve!</p>
+            <br />
+            <div style={{ marginBottom: '1rem' }}>
+              <h5 className="font-bold">Application Responsiveness</h5>
+              <p style={{ marginBottom: '0.5rem' }}>How satisfied are you with the speed and responsiveness of our mobile/web application when browsing and making purchases?</p>
+              <div className="radio-group">
+                <button
+                  className={`radio-button ${formData.q1 === "Absolutely Satisfied" ? "selected" : ""}`}
+                  onClick={() => handleChange("q1", "Absolutely Satisfied")}
+                >
+                  Absolutely Satisfied
+                </button>
+                <button
+                  className={`radio-button ${formData.q1 === "Moderately Satisfied" ? "selected" : ""}`}
+                  onClick={() => handleChange("q1", "Moderately Satisfied")}
+                >
+                  Moderately Satisfied
+                </button>
+                <button
+                  className={`radio-button ${formData.q1 === "Somewhat Dissatisfied" ? "selected" : ""}`}
+                  onClick={() => handleChange("q1", "Somewhat Dissatisfied")}
+                >
+                  Somewhat Dissatisfied
+                </button>
+              </div>
+            </div>
 
-            <InputField
-              name="q2"
-              placeholder="How satisfied are you with the approval and speed of your transaction in the system?"
-              onChange={handleChange}
-              error={validationErrors.q2 && "Field must be filled"}
-            />
-            <StarRating onClick={setq2Rate} rating={q2rating} />
+            <div>
+              <h5 className="font-bold">Order Acceptance</h5>
+              <p>How satisfied are you with the approval and speed of your transaction in the system?</p>
+              <div className="radio-group">
+                <button
+                  className={`radio-button ${formData.q2 === "Fast and Hasslefree" ? "selected" : ""}`}
+                  onClick={() => handleChange("q2", "Fast and Hasslefree")}
+                >
+                  Fast and Hasslefree
+                </button>
+                <button
+                  className={`radio-button ${formData.q2 === "Experiencing Delays but Tolerable" ? "selected" : ""}`}
+                  onClick={() => handleChange("q2", "Experiencing Delays but Tolerable")}
+                >
+                  Experiencing Delays but Tolerable
+                </button>
+                <button
+                  className={`radio-button ${formData.q2 === "Complicated and Inconvenient" ? "selected" : ""}`}
+                  onClick={() => handleChange("q2", "Complicated and Inconvenient")}
+                >
+                  Complicated and Inconvenient
+                </button>
+              </div>
+            </div>
 
-            <InputField
-              name="q3"
-              placeholder="How satisfied are you with the communication skills and punctuality of the delivery rider in delivering your LPG order?"
-              onChange={handleChange}
-              error={validationErrors.q3 && "Field must be filled"}
-            />
-            <StarRating onClick={setq3Rate} rating={q3rating} />
+            <div>
+  <h5 className="font-bold">Rider Performance</h5>
+  <p>How was the communication skills and punctuality of the delivery rider in delivering your LPG order?</p>
+  <div className="radio-group">
+    <button
+      className={`radio-button ${formData.q3 === "Interactive and Arrived on time" ? "selected" : ""}`}
+      onClick={() => handleChange("q3", "Interactive and Arrived on time")}
+    >
+      Interactive and Arrived on time
+    </button>
+    <button
+      className={`radio-button ${formData.q3 === "Average expectation" ? "selected" : ""}`}
+      onClick={() => handleChange("q3", "Average expectation")}
+    >
+      Average expectation
+    </button>
+    <button
+      className={`radio-button ${formData.q3 === "Needs more training and arrived late" ? "selected" : ""}`}
+      onClick={() => handleChange("q3", "Needs more training and arrived late")}
+    >
+      Needs more training and arrived late
+    </button>
+  </div>
+</div>
 
-            <InputField
-              name="q4"
-              placeholder="On a scale of 1 to 5, how would you describe your overall experience using our mobile/web application to purchase LPG products?"
-              onChange={handleChange}
-              error={validationErrors.q4 && "Field must be filled"}
-            />
-            <StarRating onClick={setq4Rate} rating={q4rating} />
+<div>
+  <h5 className="font-bold">Overall Satisfaction</h5>
+  <p>How would you describe your overall experience using our mobile/web application to purchase LPG products?</p>
+  <div className="radio-group">
+    <button
+      className={`radio-button ${formData.q4 === "One of a kind, will reuse the app" ? "selected" : ""}`}
+      onClick={() => handleChange("q4", "One of a kind, will reuse the app")}
+    >
+      One of a kind, will reuse the app
+    </button>
+    <button
+      className={`radio-button ${formData.q4 === "Medium performance, needs improvement" ? "selected" : ""}`}
+      onClick={() => handleChange("q4", "Medium performance, needs improvement")}
+    >
+      Medium performance, needs improvement
+    </button>
+    <button
+      className={`radio-button ${formData.q4 === "Inconvenient, will stick to conventional purchasing method." ? "selected" : ""}`}
+      onClick={() => handleChange("q4", "Inconvenient, will stick to conventional purchasing method.")}
+    >
+      Inconvenient, will stick to conventional purchasing method.
+    </button>
+  </div>
+</div>
 
-            <InputField
-              name="q5"
-              placeholder="Overall, how likely are you to recommend our mobile/web application to others based on your experience using it for LPG purchases?"
-              onChange={handleChange}
-              error={validationErrors.q5 && "Field must be filled"}
-            />
-            <StarRating onClick={setq5Rate} rating={q5rating} />
+<div>
+  <h5 className="font-bold">Recommendation</h5>
+  <p>Overall, how likely are you to recommend our mobile/web application to others based on your experience using it for LPG purchases?</p>
+  <div className="radio-group">
+    <button
+      className={`radio-button ${formData.q5 === "Will highly recommend to others" ? "selected" : ""}`}
+      onClick={() => handleChange("q5", "Will highly recommend to others")}
+    >
+      Will highly recommend to others
+    </button>
+    <button
+      className={`radio-button ${formData.q5 === "Undecided to recommend to others" ? "selected" : ""}`}
+      onClick={() => handleChange("q5", "Undecided to recommend to others")}
+    >
+      Undecided to recommend to others
+    </button>
+    <button
+      className={`radio-button ${formData.q5 === "Unlikely to recommend to others" ? "selected" : ""}`}
+      onClick={() => handleChange("q5", "Unlikely to recommend to others")}
+    >
+      Unlikely to recommend to others
+    </button>
+  </div>
+</div>
+
+
+         
 
             <div className="flex justify-center items-center p-10">
               <Button onClick={createFeedback}>Submit Feedback</Button>
@@ -174,6 +212,27 @@ export default function Feedback() {
           </>
         )}
       </div>
+
+      <style jsx>{`
+        .radio-group {
+          display: flex;
+          gap: 1rem;
+          margin-bottom: 1rem;
+        }
+
+        .radio-button {
+          flex: 1;
+          border: 1px solid #ccc;
+          padding: 10px;
+          border-radius: 5px;
+          transition: background-color 0.3s ease;
+        }
+
+        .radio-button.selected {
+          background-color: #FFFFFF;
+          border-color: #d41111;
+        }
+      `}</style>
     </main>
   );
 }
